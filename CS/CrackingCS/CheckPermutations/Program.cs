f243389711d7143ad8162653b2a47477d3071643
Sergey Okinchuk <serg3d@hotmail.com>
2016-12-11 01:48:01 -0800

    added CS project, added checkPermutations in C#

---
+++ CS/CrackingCS/CheckPermutations/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace CheckPermutations
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(checkPermutations("qwerty", "ytrewq"));
+        }
+
+        public static bool checkPermutations(string str1, string str2)
+        {
+            char[] chars1 = str1.ToCharArray();
+            char[] chars2 = str2.ToCharArray();
+
+            Array.Sort(chars1);
+            Array.Sort(chars2);
+
+            return Enumerable.SequenceEqual(chars1, chars2);
+        }
+    }
+}
